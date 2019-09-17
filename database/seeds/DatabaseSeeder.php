dfe695e976e70419b2fddc834823f3e03bf05264
AliAnilKocak <alianilkocak@gmail.com>
2019-09-17 15:02:24 +0300

    Initial commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\User::class,100)->create();
+        factory(\App\Post::class,1500)->create();
+        factory(\App\Category::class,10)->create();
+        // $this->call(UsersTableSeeder::class);
+    }
+}
